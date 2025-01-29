c72b1e8982722194b69e8a9f147fec057f70d139
codris-exe <rishab1928@outlook.com>
2025-01-29 10:58:46 +0530

    feat: server build

--- src/main/java/com/codris/Main.java
+++ src/main/java/com/codris/Main.java
@@ -2,6 +2,25 @@ package com.codris;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Initialization
+        MinecraftServer minecraftServer = MinecraftServer.init();
+
+        // Instance
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+
+        // ChunkGenerator
+        instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
+
+        // Spawn position
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 42, 0));
+        });
+
+        // Start the server on port 25565
+        minecraftServer.start("0.0.0.0", 25565);
     }
 }
